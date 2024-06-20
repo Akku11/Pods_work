e4f2a62c46557adbbea9874baaca258e678dc87d
Akanksha <>
2024-06-20 14:54:33 +0800

    added framework

---
+++ CoffeeFramework/CoffeeFramework/CoffeeTeams.swift
@@ -0,0 +1,16 @@
+//
+//  CoffeeTeams.swift
+//  CoffeeFramework
+//
+//  Created by Akanksha Thakur on 20/6/24.
+//
+
+import Foundation
+
+public class Teams {
+  public init() {}
+  
+  public func getName() {
+    print("** Ak **")
+  }
+}
